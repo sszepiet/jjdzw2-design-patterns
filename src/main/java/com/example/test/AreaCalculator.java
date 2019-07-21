510f3ed7d66b927ae450e1ec1f02eb14b30e85d9
Szymon Szepietowski <sszepiet@gmail.com>
2019-07-21 06:38:37 +0200

    Empty repo - a metaphore of human life

---
+++ src/main/java/com/example/test/AreaCalculator.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import java.util.Scanner;
+
+public class AreaCalculator {
+
+    public static void main(String[] args) {
+
+        boolean ok = true;
+        Scanner s = new Scanner(System.in);
+
+        while (ok) {
+            System.out.print("calculator > ");
+            String commandString = s.nextLine();
+            System.out.println("Command: " + commandString);
+        }
+        s.close();
+    }
+}
